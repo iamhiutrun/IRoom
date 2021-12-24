62a900f0b2f412a4f6934de74a27304cf89f8c24
Lương Trung Hiếu <hieu.luongtrung@ddsolution.vn>
2021-12-24 15:51:17 +0700

    setup naviagtion component

--- app/src/main/java/com/example/iroom/MainActivity.kt
+++ app/src/main/java/com/example/iroom/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.iroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.iroom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityMainBinding
+    private val binding get() = _binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
